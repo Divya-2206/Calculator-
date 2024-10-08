e99a8dcfcb4c38062b3bb945494da02b3be13eab
Divya-2206 <2206divya@gmail.com>
2024-10-08 20:26:01 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+ 
+public class MainClass {
+    public static void main(String[] args)
+    {
+            new Calculator();
+    }
+}
